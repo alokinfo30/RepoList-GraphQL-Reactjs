a7c7dbfdbec0cc601d9c5c3cf4ee3462a8c10cb5
alokinfo30 <alokinfo.proict@gmail.com>
2023-08-11 16:06:24 +0530

    adding files to git

---
+++ src/db.js
@@ -0,0 +1,9 @@
+const github = {
+    baseURL: "https://api.github.com/graphql",
+    username: "alokinfo30",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "bearer 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",
+    },
+  };
+  export default github;
